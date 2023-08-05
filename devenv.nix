4dd7667b29fabe3716fe7dee7c800c5ad9e88ca9
Gustavo Cardoso <self.gustavocorrea@gmail.com>
2023-08-05 16:05:49 -0300

    addind devenv

---
+++ devenv.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ git python39 poetry ];
+
+  languages.python = {
+      enable = true;
+      package = pkgs.python39;
+      poetry = {
+          enable = true; 
+          activate.enable = true; 
+          install = {enable = true; allExtras = true;};
+      };
+  };
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
